75ba8a40ca5427dd5019fd8f8e792eca87c22ac8
azarual01 <elghazi.azarual@le-campus-numerique.fr>
2023-03-24 13:50:00 +0100

    test de test

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -36,15 +36,21 @@ class MagasinTest {
                 new Item("Kryptonite", 0, 0),
                 new Item("Kryptonite", -10, -10),
 
-
                 new Item("product", 0, 0),
                 new Item("product", 0, 50),
                 new Item("product", 5, 45),
                 new Item("product", 10, 45),
                 new Item("product", 20, 20),
                 new Item("product", -10, -10),
+
+                new Item("Pouvoirs magiques", 0, 0),
+                new Item("Pouvoirs magiques", 0, 50),
+                new Item("Pouvoirs magiques", 5, 45),
+                new Item("Pouvoirs magiques", 10, 45),
+                new Item("Pouvoirs magiques", 20, 20),
+                new Item("Pouvoirs magiques", -10, -1)
         };
-        int[] expected = new int[] {2,50,46,46,21,2,-8,0,0,48,47,21,0,0,0,50,45,45,20,0,-10,0,48,44,44,19,0,-10};
+        int[] expected = new int[] {2,50,46,46,21,2,-8,0,0,48,47,21,0,0,0,50,45,45,20,0,-10,0,48,44,44,19,0,0,46,43,43,18,0};
         Magasin app = new Magasin(items);
         app.updateQuality();
         for (int i = 0; i < items.length; i++) {
